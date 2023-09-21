5039e1e7f08c1c3d4899dfb9bf9823a30ed149f2
Trevor Augustine <trevor.okinda@strathmore.edu>
2023-09-21 20:17:07 +0300

    Debug reset password feature

--- Backend/db/index.js
+++ Backend/db/index.js
@@ -3,7 +3,7 @@ mongoose
 .connect("mongodb://127.0.0.1:27017/AI-Weather-App", {
     useNewUrlParser: true,
     useUnifiedTopology: true, 
-    // useCreateIndex: true  
+    useCreateIndex: true  
 })
 .then(() => console.log("db is connected"))
 .catch((err) => console.log(err));
