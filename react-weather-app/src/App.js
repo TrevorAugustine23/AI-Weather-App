9a87bf11ecf4c50399fc936a4e378b56b05def8b
Trevor Augustine <trevor.okinda@strathmore.edu>
2023-10-25 21:07:17 +0300

    Time and Location Details

--- react-weather-app/src/App.js
+++ react-weather-app/src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import UilReact from '@iconscout/react-unicons/icons/uil-react'
 import TopButtons from './components/TopButtons';
 import Inputs from './components/Inputs';
+import TimeandLocation from './components/TimeandLocation';
 
 function App() {
   return (
    <div className="mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 h-fit shadow-xl shadow-gray-400">
     <TopButtons/>
     <Inputs/>
+    <TimeandLocation/>
    </div>
   );
 }
