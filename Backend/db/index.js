2a712de3a00be70305355198084646f4a83134af
Trevor Augustine <trevor.okinda@strathmore.edu>
2023-10-26 08:09:37 +0300

    minor update

--- Backend/db/index.js
+++ Backend/db/index.js
@@ -3,7 +3,7 @@ mongoose
 .connect("mongodb://127.0.0.1:27017/AI-Weather-App", {
     useNewUrlParser: true,
     useUnifiedTopology: true, 
-    useCreateIndex: true  
+    // createIndexes: true  
 })
 .then(() => console.log("db is connected"))
 .catch((err) => console.log(err));
