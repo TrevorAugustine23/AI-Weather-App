0e2b7c3c1dbcb8d67f3d74c30c4f0644974b3b4f
Trevor Augustine <trevor.okinda@strathmore.edu>
2023-10-28 14:31:28 +0300

    files clean-up

--- Backend/db/index.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-mongoose
-.connect("mongodb://127.0.0.1:27017/AI-Weather-App", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true, 
-    // createIndexes: true  
-})
-.then(() => console.log("db is connected"))
-.catch((err) => console.log(err));
