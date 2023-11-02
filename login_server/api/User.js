800985a78ce0012a2dbbd318b0f0742a4d547014
Trevor Augustine <trevor.okinda@strathmore.edu>
2023-11-02 15:09:01 +0300

    signup route error fixed

--- login_server/api/User.js
+++ login_server/api/User.js
@@ -20,22 +20,22 @@ router.post('/signup', (req, res) => {
             status: "FAILED",
             message: "Empty input fields!"
         });
-    }else if (!/^[a-zA-Z ]*$/.test(name)) {
+    } else if (!/^[a-zA-Z ]*$/.test(name)) {
         res.json({
             status: "FAILED",
             message: "Invalid name entered"
         })
-    }else if (!/^[\w-\.]+@([\w-]+\.)+[w-]{2,4}$/.test(email)){
+    } else if (!/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(email)){
         res.json({
             status: "FAILED",
             message: "Invalid email entered"
         })
-    }else if (!new Date(dateOfBirth).getTime()) {
+    } else if (!new Date(dateOfBirth).getTime()) {
         res.json({
             status: "FAILED",
             message: "Invalid DOB entered"
         })
-    }else if (password.length < 8){
+    } else if (password.length < 8){
         res.json({
             status: "FAILED",
             message: "Password is too short"
