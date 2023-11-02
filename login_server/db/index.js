78b62d5a699b0bf9c7b8b0b92d04403a730e47a2
Trevor Augustine <trevor.okinda@strathmore.edu>
2023-11-02 13:29:22 +0300

    install mongoose

--- login_server/db/index.js
+++ login_server/db/index.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 mongoose
-.connect("mongodb://127.0.0.1:27017/AI-Weather-App", {
+.connect("mongodb://127.0.0.1:27017/UserDB", {
     useNewUrlParser: true,
     useUnifiedTopology: true, 
     // createIndexes: true  
---
+++ login_server/server.js
@@ -0,0 +1,10 @@
+const app = require('express')();
+const port = 8000;
+
+//For accepting post form data
+const bodyParser = require('express').json;
+app.use(bodyParser());
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+})
