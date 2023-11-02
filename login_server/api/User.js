c7cc847a0ff8cb164d9789734ddc00af0157e36a
Trevor Augustine <trevor.okinda@strathmore.edu>
2023-11-02 13:37:43 +0300

    setting up routes

---
+++ login_server/api/User.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+
+//Signup
+router.post('/signup', (req, res) => {
+
+})
+//Signin
+router.post('/signup', (req, res) => {
+
+})
+
+module.exports = router;
