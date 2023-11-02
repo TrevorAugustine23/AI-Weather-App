78675e1e36a2c3a2355ac08b897f4924d1fdc063
Trevor Augustine <trevor.okinda@strathmore.edu>
2023-11-02 13:57:50 +0300

    signup route handling

--- login_server/api/User.js
+++ login_server/api/User.js
@@ -3,7 +3,40 @@ const router = express.Router();
 
 //Signup
 router.post('/signup', (req, res) => {
+    let {name, email, password, dateOfBirth} = req.body;
+    name = name.trim();
+    email = email.trim();
+    password = password.trim();
+    dateOfBirth = dateOfBirth.trim();
 
+    if(name == "" || email == "" || password == "" || dateOfBirth == ""){
+        res.json({
+            status: "FAILED",
+            message: "Empty input fields!"
+        });
+    }else if (!/^[a-zA-Z]*$/.test(name)){
+        res.json({
+            status: "FAILED",
+            message: "Invalid name entered"
+        })
+    }else if (!/^[\w-\.]+@([\w-]+\.)+[w-]{2,4}$/.test(email)){
+        res.json({
+            status: "FAILED",
+            message: "Invalid email entered"
+        })
+    }else if (!new Date(dateOfBirth).getTime()) {
+        res.json({
+            status: "FAILED",
+            message: "Invalid DOB entered"
+        })
+    }else if (password.length < 8){
+        res.json({
+            status: "FAILED",
+            message: "Password is too short"
+        })
+    } else {
+        //Checking if user already exists
+    }
 })
 //Signin
 router.post('/signup', (req, res) => {
--- login_server/server.js
+++ login_server/server.js
@@ -4,9 +4,13 @@ require('./db/index');
 const app = require('express')();
 const port = 8000;
 
+const UserRouter = require('./api/User');
+
 //For accepting post form data
 const bodyParser = require('express').json;
 app.use(bodyParser());
+
+app.use('/user' , UserRouter)
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
