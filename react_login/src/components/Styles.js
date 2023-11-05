a698f6ee8f42a699d65266f4d1fdaee4516e8ced
Trevor Augustine <trevor.okinda@strathmore.edu>
2023-11-06 02:31:52 +0300

    CSS for styled button

--- react_login/src/components/Styles.js
+++ react_login/src/components/Styles.js
@@ -54,3 +54,23 @@ export const Avatar = styled.div`
     background-position: center;
     margin: auto;
         `;
+
+export const StyledButton = styled(Link)`
+    padding: 10px;
+    width: 150px;
+    background-color: transparent;
+    font-size: 16px;
+    border: 3px solid ${colors.primary};
+    border-radius: 25px;
+    color: ${colors.primary};
+    text-decoration: none;
+    text-align: center;
+    transition: ease-in-out 0.3s;
+    
+    &:hover {
+        background-color: ${colors.primary};
+        color: ${colors.theme};
+        cursor: pointer;
+
+    }
+    `;
