c355a2e07fa816d9953d363df92265c62b657f91
Trevor Augustine <trevor.okinda@strathmore.edu>
2023-11-06 01:46:02 +0300

    installed styled components and set project colors

---
+++ react_login/src/components/Styles.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const colors ={
+    primary: "#fff",
+    theme: "#BE185D",
+    light1: "#F3F4F6",
+    light2: "#E5E7EB",
+    dark1: "#1F2937",
+    dark2: "#4B5563",
+    dark3: "#9CA3AF",
+    red: "#DC2626"
+}
