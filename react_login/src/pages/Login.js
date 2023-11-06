84e2a2b810a316ed660d41eaec5c76c1c05bd523
Trevor Augustine <trevor.okinda@strathmore.edu>
2023-11-06 03:38:49 +0300

    created login email and password input fields

--- react_login/src/pages/Login.js
+++ react_login/src/pages/Login.js
@@ -4,6 +4,8 @@ import {StyledTextInput, StyledFormArea, StyledFormButton, StyledLabel, Avatar, 
 import Logo from "./../assets/favicon.png";
 
 //formik
+import { Formik, Form } from "formik";
+import { TextInput } from "../components/FormLib";
 
 
 const Login = () => {
@@ -12,7 +14,24 @@ const Login = () => {
             <StyledFormArea>
                 <Avatar image={Logo}></Avatar>
                 <StyledTitle color={colors.theme} size={30}>Farmer Login</StyledTitle>
-
+                <Formik>
+                    {() => (
+                        <Form>
+                            <TextInput
+                                name="email"
+                                type="text"
+                                label="Email Address"
+                                placeholder="enter@example.com"
+                            />
+                            <TextInput
+                                name="password"
+                                type="password"
+                                label="Password"
+                                placeholder="enter password"
+                            />
+                        </Form>
+                    )}
+                </Formik>
             </StyledFormArea>
         </div>
     )
