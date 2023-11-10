fe375807b58e3fa33e741cb905dff3a0c137b966
Trevor Augustine <trevor.okinda@strathmore.edu>
2023-11-10 21:32:01 +0300

    set up axios and session

--- react_login/src/auth/actions/userActions.js
+++ react_login/src/auth/actions/userActions.js
@@ -1,24 +1,48 @@
 import axios from 'axios';
+import { ErrorMessage } from 'formik';
+
+import { sessionService } from 'redux-react-session';
 
 const API_BASE_URL = 'http://localhost:8000/api'; // Replace with your server's URL
 
 export const loginUser = (credentials, history, setFieldError, setSubmitting) => {
-    axios.post(`${API_BASE_URL}/login`, credentials)
-        .then(response => {
-            // Handle the successful login response
-            console.log('Login successful:', response.data);
-            // Redirect the user or perform further actions upon successful login
-        })
-        .catch(error => {
-            // Handle login error
-            console.error('Login error:', error);
-            // Set error messages using setFieldError if needed
-        });
+    axios.post(`${API_BASE_URL}/login`, 
+    credentials,
+    {
+        headers:{
+            "Content-Type": "application.json"
+        }
+    }
+    ).then(response => {
+            const {data} = response;
+
+            if(data.status === "FAILED") {
+                const {message} = data;
+
+                //check for specific error
+                if( message.includes ("credentials")) {
+                    setFieldError("email", message);
+                    setFieldError("password", message);
+                } else if( message.includes ("password")) {
+                    setFieldError("password", message);
+                }
+            } else if (data.status === "SUCCESS") {
+                const userData = data.data[0];
+
+                const token = userData._id;
+
+                sessionService.saveSession(token).then(() => {
+                    sessionService.saveUser(userData).then(() => {
+
+                    }).catch(err => console.error(err))
+                }).catch(err => console.error(err))
+            }
+    }).catch(err => console.error(err))
 }
 
 export const signupUser = (credentials, history, setFieldError, setSubmitting) => {
     
 }
 export const logoutUser = () => {
-    
+     
 }
