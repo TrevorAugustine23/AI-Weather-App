dd621c3ecae7a8588a929228baa9184293795fb6
Trevor Augustine <119278257+TrevorAugustine23@users.noreply.github.com>
2023-11-29 13:35:51 +0300

    Update apiKeys.js

--- weatherApp-Reactjs/src/apiKeys.js
+++ weatherApp-Reactjs/src/apiKeys.js
@@ -1,4 +1,4 @@
-// Visit https://api.openweathermap.org & then signup to get our API keys for free
+
 module.exports = {
   key: "278c14ab5a309be7679669098da8640e",
   base: "https://api.openweathermap.org/data/2.5/",
